cbf1761c0d8ea6e7b211e4804900538ac1c3265e
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-11-20 16:30:15 +0100

    created hero text placeholder

--- tar-frontend/app/page.tsx
+++ tar-frontend/app/page.tsx
@@ -1,7 +1,10 @@
+import Hero from "@/components/shared/Hero";
+
 export default function Home() {
   return (
     <section className="bg-slate-950 flex justify-center items-center h-screen w-screen">
-      <h1 className="text-white text-8xl font-heart">Hi there</h1>
+      {/* <h1 className="text-white text-8xl font-heart">Hi there</h1> */}
+      <Hero />
     </section>
   );
 }
--- tar-frontend/components/shared/Hero.tsx
+++ tar-frontend/components/shared/Hero.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <article className="w-screen h-auto flex justify-center items-center">
+      <div className="text-main-primary font-motter">
+        <h1 className="flex flex-col text-center font-s text-6xl">
+          <span>DRINK.</span>
+          <span>EAT.</span>
+          <span>RELAX.</span>
+        </h1>
+        <p className="text-base uppercase">Pizza like you never had it</p>
+      </div>
+    </article>
+  );
 };
 
 export default Hero;
