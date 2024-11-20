f226f949aff2a1dbe00cdeb185c705aa27e7e187
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-11-20 15:56:37 +0100

    created hero file under shared folder

--- tar-frontend/app/page.tsx
+++ tar-frontend/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <section className="flex justify-center items-center h-screen w-screen">
+    <section className="bg-slate-950 flex justify-center items-center h-screen w-screen">
       <h1 className="text-white text-8xl font-heart">Hi there</h1>
     </section>
   );
