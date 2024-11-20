c8ae5ef98d111096ff87d086e71ddc85af3b8263
Simon Gustavsson <simgus1234@gmail.com>
2024-11-20 21:07:03 +0100

    Updated animation delay

--- tar-frontend/components/shared/Nav.tsx
+++ tar-frontend/components/shared/Nav.tsx
@@ -188,8 +188,8 @@ const Nav = () => {
                         initial={{ opacity: 0, x: "100%" }}
                         animate={{ opacity: 1, x: 0 }}
                         transition={{
-                          duration: 0.7,
-                          delay: index * 0.1,
+                          duration: 0.5,
+                          delay: index * 0.05,
                           ease: "backInOut",
                         }}
                         onClick={toggleOpen}
