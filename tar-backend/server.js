559e8e8a7316ff6aee3c48422dcab97accd7d780
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-11-21 19:49:57 +0100

    Add constant fold

--- tar-backend/server.js
+++ tar-backend/server.js
@@ -1,18 +1,21 @@
-const { default: mongoose } = require("mongoose");
-const app = require("./app");
+const { default: mongoose } = require('mongoose');
+const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+// This is the entry point of the application
+const PORT = process.env.PORT || 8000;
 
+// This is the connection-url to the database
 const { ATLAS_URI } = process.env;
 
+// This is the connection to the database
 mongoose
   .connect(ATLAS_URI)
   .then(() => {
-    console.log("Connected to database");
+    console.log('Connected to database');
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log("Error", err);
+    console.log('Error', err);
   });
