f7693b9a956ebcad90fce4355c4ff21f1e1e7c47
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-11-21 19:49:57 +0100

    responsive spinning-scroll

--- tar-frontend/app/page.tsx
+++ tar-frontend/app/page.tsx
@@ -1,16 +1,13 @@
 import Hero from "@/components/shared/Hero";
+import { img } from "motion/react-client";
 
 export default function Home() {
   const description = "pizza like you never had it";
   const title = ["DRINK.", "EAT.", "RELAX."];
+
   return (
     <section className="bg-slate-950 flex justify-center items-center h-screen w-screen">
-      <Hero
-        description={description}
-        homePage
-        title={title}
-        className="bg-main-light"
-      />
+      <Hero description={description} homePage title={title} />
     </section>
   );
 }
