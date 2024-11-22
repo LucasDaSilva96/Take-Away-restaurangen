6bfaf5378ff48ad55bca444c9c8f9e973f747302
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-11-22 11:13:21 +0100

    added point finger and bounce animation

--- tar-frontend/app/page.tsx
+++ tar-frontend/app/page.tsx
@@ -1,12 +1,24 @@
 import Hero from "@/components/shared/Hero";
-import { img } from "motion/react-client";
+// import { img } from "motion/react-client";
+import { hero_video } from "@/constants/constants";
 
 export default function Home() {
   const description = "pizza like you never had it";
   const title = ["DRINK.", "EAT.", "RELAX."];
+  const video = (
+    <video
+      className="w-full h-full absolute object-cover z-0"
+      src={hero_video}
+      autoPlay
+      muted
+      loop
+    ></video>
+  );
 
   return (
-    <section className="bg-slate-950 flex justify-center items-center h-screen w-screen">
+    <section className="bg-slate-950 flex justify-center items-center h-screen w-screen ">
+      {video}
+
       <Hero description={description} homePage title={title} />
     </section>
   );
