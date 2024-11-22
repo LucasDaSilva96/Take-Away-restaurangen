f203c426caa58377f7a30eeed3eb175eb9df1ec4
Jens Alm <jensalm@Jenss-MacBook-Pro.local>
2024-11-22 10:12:21 +0100

    Created SectionA

--- tar-frontend/app/page.tsx
+++ tar-frontend/app/page.tsx
@@ -1,17 +1,31 @@
 import Hero from "@/components/shared/Hero";
+import SectionA from "@/components/shared/SectionA";
 
 export default function Home() {
-  const description = "pizza like you never had it";
-  const title = ["DRINK.", "EAT.", "RELAX."];
+  const heroDescription = "pizza like you never had it";
+  const heroTitle = ["DRINK.", "EAT.", "RELAX."];
+  const sectionADescription =
+    "Feeling snacky? Experience the joy of a great pizza like never before. Let us guide you through a sensation of flavours. Come get lucky!";
+  const sectionATitle = [
+    "FEELING SNACKY?",
+    "EAT GREAT PIZZA!",
+    "COME GET LUCKY!",
+  ];
+
   return (
-    <section className="bg-slate-950 flex justify-center items-center h-screen w-screen">
-      {/* <h1 className="text-white text-8xl font-heart">Hi there</h1> */}
-      <Hero
-        description={description}
-        homePage
-        title={title}
-        className="bg-main-light"
-      />
-    </section>
+    <main className="bg-slate-950">
+      {/* Hero Section */}
+      <section className="flex justify-center items-center h-screen w-screen">
+        <Hero
+          description={heroDescription}
+          homePage
+          title={heroTitle}
+          className="bg-main-light"
+        />
+      </section>
+
+      {/* Section A */}
+      <SectionA description={sectionADescription} className="bg-main-dark" />
+    </main>
   );
 }
