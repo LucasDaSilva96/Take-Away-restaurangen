2e4760c866c1eb0d2a6535aef850a884f157fb70
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-11-29 13:54:30 +0100

    Finish creating the helper functions for the API calls

--- tar-frontend/util/menu.ts
+++ tar-frontend/util/menu.ts
@@ -33,7 +33,7 @@ export async function getMenuById(id: string) {
 // createMenu is an async function that makes a POST request to the /menu endpoint of the backend API. It takes a Menu_Get object as an argument and returns the created Menu_Get object.
 export async function createMenu(formData: FormData) {
   const menu: Menu_Post = {
-    title: formData.get('name') as string,
+    title: formData.get('title') as string,
     description: formData.get('description') as string,
     price: parseFloat(formData.get('price') as string),
     image: formData.get('image') as File | undefined,
@@ -52,3 +52,56 @@ export async function createMenu(formData: FormData) {
     throw new Error(catchError(error));
   }
 }
+
+// getSingleMenu is an async function that makes a GET request to the /menu/:id endpoint of the backend API. It returns a single Menu_Get object.
+export const getSingleMenu = async (id: string) => {
+  try {
+    const response = await axios.get<Menu_Get>(BASE_API_URL + `/menu/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(catchError(error));
+  }
+};
+
+// updateMenu is an async function that makes a PUT request to the /menu/:id endpoint of the backend API. It takes a Menu_Get object as an argument and returns the updated Menu_Get object.
+type MenuPatch = {
+  id: string;
+  formData: FormData;
+};
+export const updateMenu = async ({ formData, id }: MenuPatch) => {
+  const menu: Menu_Post = {
+    title: formData.get('name') as string,
+    description: formData.get('description') as string,
+    price: parseFloat(formData.get('price') as string),
+    image: formData.get('image') as File | undefined,
+    category: (formData.get('category') as string) || 'Pizza',
+    ingredients: (formData.get('ingredients') as string).split(','),
+    inventory: parseInt(formData.get('inventory') as string),
+    quantity: parseInt(formData.get('quantity') as string),
+    onSale: formData.get('onSale') === 'true',
+  };
+  try {
+    const response = await axios.patch<Menu_Get>(
+      BASE_API_URL + `/menu/${id}`,
+      menu
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(catchError(error));
+  }
+};
+
+// deleteMenu is an async function that makes a DELETE request to the /menu/:id endpoint of the backend API. It takes an id as an argument and returns a success message.
+export const deleteMenu = async (id: string) => {
+  try {
+    const response = await axios.delete<{ message: string }>(
+      BASE_API_URL + `/menu/${id}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(catchError(error));
+  }
+};
