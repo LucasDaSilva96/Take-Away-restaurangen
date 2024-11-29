39ab9e0825fd10a03e3a603f53439ae6c9d41d54
Simon Gustavsson <simgus1234@gmail.com>
2024-11-29 10:36:35 +0100

    Fixed layout

--- tar-frontend/app/layout.tsx
+++ tar-frontend/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${Motter.variable} ${Heart.variable} ${Alumni.variable} bg-main-light antialiased`}
+        className={`${Motter.variable} ${Heart.variable} ${Alumni.variable} bg-main-light antialiased overflow-x-hidden no-scrollbar`}
       >
         <Nav />
         <Cartoverlay />
