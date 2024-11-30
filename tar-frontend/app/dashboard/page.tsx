821e658c381b51637f39fba02f979dcd495054f4
Simon Gustavsson <simgus1234@gmail.com>
2024-11-30 10:35:40 +0100

    Ordercomponent Done

--- tar-frontend/app/dashboard/page.tsx
+++ tar-frontend/app/dashboard/page.tsx
@@ -1,6 +1,63 @@
-import React from 'react';
+import OrderItem from "@/components/Dashboard/OrderItem";
+import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  const orders = [
+    {
+      id: "1",
+      status: "Pending",
+      timeStamp: "24.05",
+      locked: false,
+    },
+    {
+      id: "2",
+      status: "Pending",
+      timeStamp: "24.05",
+      locked: false,
+    },
+    {
+      id: "3",
+      status: "Pending",
+      timeStamp: "24.05",
+      locked: false,
+    },
+    {
+      id: "4",
+      status: "Pending",
+      timeStamp: "24.05",
+      locked: false,
+    },
+    {
+      id: "5",
+      status: "Pending",
+      timeStamp: "24.05",
+      locked: false,
+    },
+    {
+      id: "6",
+      status: "Pending",
+      timeStamp: "24.05",
+      locked: false,
+    },
+  ];
+  return (
+    <section className="w-full flex justify-center items-center">
+      <section className="w-full aspect-square bg-white">
+        <p className="text-black">Orders</p>
+
+        <section className="flex flex-col justify-start items-start gap-2 p-6">
+          {orders.map((order) => (
+            <OrderItem
+              key={order.id}
+              id={order.id}
+              locked={order.locked}
+              status={order.status}
+              timeStamp={order.timeStamp}
+            />
+          ))}
+        </section>
+      </section>
+    </section>
+  );
 };
 export default Page;
