1639727b37fb280757179cc90a2d317faef9ea25
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-12-09 21:12:14 +0100

    New merge with devstate

--- tar-frontend/app/dashboard/page.tsx
+++ tar-frontend/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
 const Page = () => {
   return (
-    <section className="w-full flex justify-center items-center">
-      <section className="w-full aspect-square bg-transparent p-4">
-        <p className="text-black">Landning</p>
+    <section className='w-full flex justify-center items-center'>
+      <section className='w-full aspect-square bg-transparent p-4'>
+        <p className='text-black'>Landning</p>
       </section>
     </section>
   );
