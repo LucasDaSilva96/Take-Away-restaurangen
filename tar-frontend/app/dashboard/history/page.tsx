abd3f93dcf5f4abeb42357bb7b2dbbfb405c800e
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-12-11 09:39:13 +0100

    Working on orderHistory

--- tar-frontend/app/dashboard/history/page.tsx
+++ tar-frontend/app/dashboard/history/page.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 const Page = () => {
   const router = useRouter();
   const { user } = useCart();
+
   return (
     <section className="w-full flex flex-col justify-start items-start p-4 gap-2">
       <section className="flex justify-center items-center w-full text-black font-motter text-2xl text-center">
@@ -17,7 +18,40 @@ const Page = () => {
 
       <section className="w-full flex flex-col">
         {user.orders.length > 0 ? (
-          user.orders.map(() => <section></section>)
+          user.orders.map((order) => (
+            <section className="w-full h-44 bg-main-secondary ">
+              {/* Columns for order history details */}
+              <div className="border-2 border-main-moss flex justify-between text-xl  lg:text-2xl">
+                <div className=" w-full">
+                  <h2 className="text-center text-main-primary">Order ID</h2>
+                </div>
+                <div className="border-x-2 border-main-moss w-full">
+                  <h2 className="text-center  text-main-primary">Menu</h2>
+                </div>
+                <div className=" w-full">
+                  <h2 className="text-center  text-main-primary">Price</h2>
+                </div>
+              </div>
+              {/* Order history details */}
+              <div className=" flex justify-between text-xl  lg:text-2xl">
+                <div className=" w-full">
+                  <p className="text-center text-sm  text-main-primary">
+                    {order.id}
+                  </p>
+                </div>
+                <div className="w-full">
+                  <p className="text-center text-sm  text-main-primary">
+                    Måste fixa här
+                  </p>
+                </div>
+                <div className=" w-full">
+                  <p className="text-center text-sm  text-main-primary">
+                    {order.total}
+                  </p>
+                </div>
+              </div>
+            </section>
+          ))
         ) : (
           <div className="w-full flex flex-col justify-center items-center gap-2">
             <p className="text-black font-motter">Oops no orders here!</p>
